72795a0fca30b55d45e3bb4f867b097ca046bed4
couldbejake <couldbejake@gmail.com>
2019-06-11 13:51:58 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,46 +1,31 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-  <title>Pinewood House</title>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+    <title>Pinewood House</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
 </head>
+
 <body>
 
-<div class="jumbotron text-center">
-  <h1>Pinewood House</h1>
-  <p>People who have visited today</p> 
-</div>
-  
-<div class="container">
+    <div class="jumbotron text-center">
+        <h1>Pinewood House</h1>
+        <p>People who have visited today</p>
+    </div>
 
-<?php
-$files = scandir("images/" . date('Y-m-d'));
+    <div class="container">
 
-$files = array_reverse($files);
+        <?php $files=s candir( "images/" . date( 'Y-m-d')); $files=a rray_reverse($files); foreach($files as $file){ if (strpos($file, '.jpg') !==f alse) { $without_extension=s ubstr($file, 0, strrpos($file, ".")); echo "<div class='col-sm-4'>"; echo "<p>" . str_replace( "-", ":",$without_extension) . "</p>"; echo "<img src='/doorbell/images/" . date( 'Y-m-d') . "/". $file . "' style='width:100%;'>"; echo "</div>"; } } ?>
 
-foreach($files as $file){
+    </div>
 
-if (strpos($file, '.jpg') !== false) {
-    $without_extension = substr($file, 0, strrpos($file, "."));
-    echo "<div class='col-sm-4'>";
-    echo "<p>" . str_replace("-",":",$without_extension) . "</p>";
-    echo "<img src='/doorbell/images/" . date('Y-m-d') . "/". $file ."' style='width:100%;'>";
-    echo "</div>";
-
-}
-}
-
-?>
-
-</div>
-
-<br><br><br>
+    <br><br><br>
 
 </body>
-</html>
 
 </html>
+
